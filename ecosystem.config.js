9c14dad2efd3876e3cebf744b4e6f0da4dac0711
m4nishh <manish64cse@gmail.com>
2023-02-01 00:02:26 +0530

    added ecosystem config

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apps : [
+        {
+          name: "beta-bawse-backend",
+          script: "npm start",
+          watch: true,
+          log_date_format : "YYYY-MM-DD HH:mm Z",
+        }
+    ]
+}
